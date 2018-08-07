9a0f0873f779f0090f9fdca45268619df87ccfa6
bentoks2016 <nwogbobaret@gmail.com>
2018-08-07 14:38:41 -0400

    Image field added to Products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180806175401) do
+ActiveRecord::Schema.define(version: 20180807183616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180806175401) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
